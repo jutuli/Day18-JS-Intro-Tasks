f67eba332c898d1e9eb6edfef523a034f409254c
Julia T <juliaturkali@gmail.com>
2024-12-09 13:33:56 +0100

    Task Output

---
+++ Output/assets/js/output.js
@@ -0,0 +1,21 @@
+//  ========= TASK Output-Level-1_4 ========
+
+// window.alert("Hallo Welt");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Ihr Name");
+
+// window.confirm("Stummen Sie meiner Meinung zu?");
+
+// let alter = window.prompt("Gib bitte dein Alter ein:");
+// console.log(alter);
+
+// let b = 5;
+// let a = b;
+// b = 5 - 3;
+// window.alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt: ";
+// let grosse = "2.72m";
+// window.confirm(mann + grosse);
