f2449b9724b4e185656ecdc3c009f9b13e623dcf
Julia T <juliaturkali@gmail.com>
2024-12-09 13:18:22 +0100

    Arithmetic Operators Tasks

---
+++ ArithmetischeOperatoren/assets/js/arithmeticOperators.js
@@ -0,0 +1,60 @@
+// ============= TASK INTRO-Level-1_8 =============
+
+let x = 20;
+let y = 30;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+let z = 10;
+console.log((x * y) / z);
+
+let a = 15;
+let b = 9;
+
+console.log(a % b);
+
+let c = 20;
+let resultTwo = (a + b) * c;
+
+console.log(resultTwo);
+
+// =========== TASK INTRO-Level-1_2 =============
+
+let score = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+let score1 = (5 + 5) * 10;
+console.log("Ergebnis: " + score1);
+
+let score2 = 0;
+let score3 = score2 + 10;
+console.log("Ergebnis: " + score3);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+zahl++;
+console.log("increment: " + zahl);
+
+zahl--;
+console.log("decrement: " + zahl);
+
+// =========== TASK INTRO-Level-1_13 =============
+// Addition
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+// Subtraktion
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+// Multiplikation
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+// Division
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+// Modul: zeigt an, was der Rest sein würde.
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
