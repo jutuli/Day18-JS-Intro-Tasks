66cc8c9d8d7eb94dd2680bbb1239a1e42f310b45
Julia T <juliaturkali@gmail.com>
2024-12-09 13:45:01 +0100

    Task Strings

---
+++ Strings/assets/js/strings.js
@@ -0,0 +1,18 @@
+// ========= String-Methods-Level-1_11 =========
+
+let x = "Hello";
+let y = "World";
+let z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+
+document.write(n);
+
+x += "World";
+document.write(x);
+
+let meinString = "Ich bin Erste.";
+meinString += " Ich komme auf Platz zwei.";
+console.log(meinString);
