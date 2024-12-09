fa51cac684161c1641038eeef0ec5e7f7b59542d
Julia T <juliaturkali@gmail.com>
2024-12-09 13:26:04 +0100

    Task Variablen

---
+++ Variablen/assets/js/variablen.js
@@ -0,0 +1,43 @@
+//  ============ TASK  Variablen-Level-1_5 ===========
+
+let carName = "BMW";
+console.log(carName);
+
+x = 150;
+console.log(x);
+
+y = 50;
+console.log(y);
+
+z = x + y;
+console.log(z);
+
+let firstName;
+let lastName;
+let age;
+
+firstName = "John";
+lastName = "Doe";
+age = 35;
+
+console.log(`${firstName} ${lastName}, ${age}`);
+
+//  ============ TASK  Variablen-Level-1_6 ===========
+
+let x1 = 34;
+x1 = 67;
+console.log(x1);
+
+const y1 = 34;
+y1 = 67;
+console.log(y1);
+
+//  ============ TASK  Variablen-Level-1_7 ===========
+
+let helloWorld = "Hello World";
+const pi = 3.14;
+const burjDubai = "828m";
+let fullName = "Jan Schmidt";
+const eifelTower = "324m";
+const camelCase = "camelCase";
+let kursStatus = true;
